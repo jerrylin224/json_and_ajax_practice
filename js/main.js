440d5853cf6f21bbbe92ac95f437cd2b83f36820
Mata Angelou <pk60905@hotmail.com>
2017-04-24 15:29:04 +0800

    Add a button and able to show weather based on zip

--- js/main.js
+++ js/main.js
@@ -6,10 +6,27 @@ var weatherForecast = new XMLHttpRequest();
 var cObj;
 var fObj;
 
-// GET THE CONDITIONS
-weatherConditions.open('GET', 'http://api.wunderground.com/api/5d942397e92591f2/conditions/q/CA/San_Francisco.json', true);
-weatherConditions.responseType = 'text';
-weatherConditions.send(null);
+function loadWeather() {
+  var zip = document.getElementById('zip').value;
+  if (zip === "") { zip = 84653; }
+
+  var conditionsPath = "http://api.wunderground.com/api/5d942397e92591f2/conditions/q/CA/" + zip + ".json";
+  var forecastPath = "http://api.wunderground.com/api/5d942397e92591f2/forecast/q/CA/" + zip + ".json";
+
+  // GET THE CONDITIONS
+  weatherConditions.open('GET', conditionsPath, true);
+  weatherConditions.responseType = 'text';
+  weatherConditions.send(null);
+
+  // GET THE FORECARST
+  weatherForecast.open('GET', forecastPath, true);
+  weatherForecast.responseType = 'text'; 
+  weatherForecast.send();
+}
+
+
+
+
 
 weatherConditions.onload = function() {
     if (weatherConditions.status === 200){
@@ -17,18 +34,13 @@ weatherConditions.onload = function() {
         console.log(cObj);
         document.getElementById('location').innerHTML = cObj.current_observation.display_location.full;
         document.getElementById('weather').innerHTML = cObj.current_observation.weather;
-        document.getElementById('temperature').innerHTML = cObj.current_observation.temp_f;
+        document.getElementById('temperature').innerHTML = cObj.current_observation.temp_c;
 
     } //end if
 }; //end function
 
 
 
-
-// GET THE FORECARST
-weatherForecast.open('GET', 'http://api.wunderground.com/api/5d942397e92591f2/forecast/q/CA/San_Francisco.json', true);
-weatherForecast.responseType = 'text'; 
-weatherForecast.send();
 
 weatherForecast.onload = function() {
 if (weatherForecast.status === 200){
@@ -38,26 +50,26 @@ if (weatherForecast.status === 200){
 
   // day1
 	document.getElementById('r1c1').innerHTML = fObj.forecast.simpleforecast.forecastday[1].date.weekday;
-  document.getElementById('r1c3').innerHTML = fObj.forecast.simpleforecast.forecastday[1].high.fahrenheit + "°";
-  document.getElementById('r1c4').innerHTML = fObj.forecast.simpleforecast.forecastday[1].low.fahrenheit + "°";
+  document.getElementById('r1c3').innerHTML = fObj.forecast.simpleforecast.forecastday[1].high.celsius + "°";
+  document.getElementById('r1c4').innerHTML = fObj.forecast.simpleforecast.forecastday[1].low.celsius + "°";
   var imagePath1 = fObj.forecast.simpleforecast.forecastday[1].icon_url;
   document.getElementById('r1c2').src = imagePath1;
 
   // day2
   document.getElementById('r2c1').innerHTML = fObj.forecast.simpleforecast.forecastday[2].date.weekday;
-  document.getElementById('r2c3').innerHTML = fObj.forecast.simpleforecast.forecastday[2].high.fahrenheit + "°";
-  document.getElementById('r2c4').innerHTML = fObj.forecast.simpleforecast.forecastday[2].low.fahrenheit + "°";
+  document.getElementById('r2c3').innerHTML = fObj.forecast.simpleforecast.forecastday[2].high.celsius + "°";
+  document.getElementById('r2c4').innerHTML = fObj.forecast.simpleforecast.forecastday[2].low.celsius + "°";
   var imagePath2 = fObj.forecast.simpleforecast.forecastday[2].icon_url;
   document.getElementById('r2c2').src = imagePath2;
 
   // day3
   document.getElementById('r3c1').innerHTML = fObj.forecast.simpleforecast.forecastday[3].date.weekday;
-  document.getElementById('r3c3').innerHTML = fObj.forecast.simpleforecast.forecastday[3].high.fahrenheit + "°";
-  document.getElementById('r3c4').innerHTML = fObj.forecast.simpleforecast.forecastday[3].low.fahrenheit + "°";
+  document.getElementById('r3c3').innerHTML = fObj.forecast.simpleforecast.forecastday[3].high.celsius + "°";
+  document.getElementById('r3c4').innerHTML = fObj.forecast.simpleforecast.forecastday[3].low.celsius + "°";
   var imagePath3 = fObj.forecast.simpleforecast.forecastday[3].icon_url;
   document.getElementById('r3c2').src = imagePath3;
   // document.getElementById('r3c1').innerHTML = fObj.forecast.simpleforecast.forecastday[3].date.weekday;
 } //end if
 }; //end function
 
-
+loadWeather();
