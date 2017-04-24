7a0af24fc63e499fc73bbb92f5c611e5c3c96bab
Mata Angelou <pk60905@hotmail.com>
2017-04-24 15:06:09 +0800

    Add description and fowllowing three days forecast

--- js/main.js
+++ js/main.js
@@ -34,7 +34,29 @@ weatherForecast.onload = function() {
 if (weatherForecast.status === 200){
 	fObj = JSON.parse(weatherForecast.responseText);
 	console.log(fObj);
-	
+  document.getElementById('desc').innerHTML = fObj.forecast.txt_forecast.forecastday[0].fcttext;
+
+  // day1
+	document.getElementById('r1c1').innerHTML = fObj.forecast.simpleforecast.forecastday[1].date.weekday;
+  document.getElementById('r1c3').innerHTML = fObj.forecast.simpleforecast.forecastday[1].high.fahrenheit + "°";
+  document.getElementById('r1c4').innerHTML = fObj.forecast.simpleforecast.forecastday[1].low.fahrenheit + "°";
+  var imagePath1 = fObj.forecast.simpleforecast.forecastday[1].icon_url;
+  document.getElementById('r1c2').src = imagePath1;
+
+  // day2
+  document.getElementById('r2c1').innerHTML = fObj.forecast.simpleforecast.forecastday[2].date.weekday;
+  document.getElementById('r2c3').innerHTML = fObj.forecast.simpleforecast.forecastday[2].high.fahrenheit + "°";
+  document.getElementById('r2c4').innerHTML = fObj.forecast.simpleforecast.forecastday[2].low.fahrenheit + "°";
+  var imagePath2 = fObj.forecast.simpleforecast.forecastday[2].icon_url;
+  document.getElementById('r2c2').src = imagePath2;
+
+  // day3
+  document.getElementById('r3c1').innerHTML = fObj.forecast.simpleforecast.forecastday[3].date.weekday;
+  document.getElementById('r3c3').innerHTML = fObj.forecast.simpleforecast.forecastday[3].high.fahrenheit + "°";
+  document.getElementById('r3c4').innerHTML = fObj.forecast.simpleforecast.forecastday[3].low.fahrenheit + "°";
+  var imagePath3 = fObj.forecast.simpleforecast.forecastday[3].icon_url;
+  document.getElementById('r3c2').src = imagePath3;
+  // document.getElementById('r3c1').innerHTML = fObj.forecast.simpleforecast.forecastday[3].date.weekday;
 } //end if
 }; //end function
 
